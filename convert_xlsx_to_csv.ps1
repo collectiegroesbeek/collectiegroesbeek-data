cde99d62c5aab24c480d0d69bad8919d6d4b861d
Frank <>
2018-09-26 13:21:34 +0200

    Powershell script to convert xlsx to csv

---
+++ convert_xlsx_to_csv.ps1
@@ -0,0 +1,23 @@
+# Powershell script that converts Excel files to csv.
+# The csv output is Tab delimited UTF-16LE with leading Byte Order Mark.
+
+# Put in the path here:
+$path = ''
+
+$excel = new-object -ComObject "Excel.Application"
+$excel.DisplayAlerts = $True
+$excel.Visible = $false
+
+Get-ChildItem $path -Filter *.xlsx | Foreach-Object{
+    'processing ' + $_.FullName
+    $wb = $excel.Workbooks.Open($_.FullName)
+    $dst_file = $path + "\" + $_.BaseName + ".csv"
+    Remove-Item –path $dst_file
+    $wb.SaveAs($dst_file, 42)  # 42 is Tab delimited UTF-16LE with leading Byte Order Mark
+    'saved ' + $dst_file
+    $wb.Close($True)
+    Start-Sleep -Seconds 2
+}
+
+$excel.Quit()
+[void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel)
