7351796728f23db1e7eb1de0ff82fbd3e0f3df98
Frank <33519926+Conengmo@users.noreply.github.com>
2020-03-15 15:20:19 +0100

    Convert xlsx to csv with python, not powershell

---
+++ convert_to_csv.py
@@ -0,0 +1,15 @@
+import os
+
+from xlsx2csv import Xlsx2csv
+
+
+def main():
+    for filename in sorted(os.listdir('.')):
+        if filename.endswith('.xlsx'):
+            print(f'Converting {filename} to csv')
+            filename_csv = filename.replace('.xlsx', '.csv')
+            Xlsx2csv(filename, outputencoding='utf-8').convert(filename_csv)
+
+
+if __name__ == '__main__':
+    main()
