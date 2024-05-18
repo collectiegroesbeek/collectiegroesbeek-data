9357ab799e17ce24354109dfbf3406fdfb04f11f
Frank <33519926+Conengmo@users.noreply.github.com>
2024-05-18 09:40:12 +0200

    create csv with os line separator

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -4,12 +4,17 @@ from xlsx2csv import Xlsx2csv
 
 
 def main():
-    for filename in sorted(os.listdir('.')):
-        if filename.endswith('.xlsx'):
-            print(f'Converting {filename} to csv')
-            filename_csv = filename.replace('.xlsx', '.csv')
-            Xlsx2csv(filename, outputencoding='utf-8', skip_empty_lines=True).convert(filename_csv)
+    for filename in sorted(os.listdir(".")):
+        if filename.endswith(".xlsx"):
+            print(f"Converting {filename} to csv")
+            filename_csv = filename.replace(".xlsx", ".csv")
+            Xlsx2csv(
+                filename,
+                outputencoding="utf-8",
+                skip_empty_lines=True,
+                lineterminator=os.linesep,
+            ).convert(filename_csv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
